80bec7f0b34d6c7ab0f32e0a7c0ac0ebdff2ec04
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2023-10-11 10:35:28 +0200

    empezamos a crear el js

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    console.log("JS bien enlazado"); 
+    
+
+    const btn = document.getElementById("btn");
+    const areaShow = document.getElementById("area");
+    const perimeterShow = document.getElementById("perimeter");
+    const diagonalShow = document.getElementById("diagonal");
+
+
+});
