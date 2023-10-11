65ff45ecc883c4c3f5fd8ccc9ed544d21d3ecb0e
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2023-10-11 10:37:27 +0200

    acabamos el js con un if

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
         const width = parseFloat(document.getElementById("width").value);
         
 
+        if (isNaN(lenght) || isNaN(width)) {
+            areaShow.innerHTML = "";
+            perimeterShow.innerHTML = "Por favor, ingrese valores válidos"; 
+            diagonalShow.innerHTML = "";
+        } else {
+            
 
+            const area = lenght * width;
+            const perimeter = 2 * (lenght + width);
+            const diagonal = Math.sqrt(Math.pow(lenght, 2) + Math.pow(width, 2));
+
+
+            areaShow.innerHTML = `Área del rectángulo: ${area.toFixed(2)}`;
+            perimeterShow.innerHTML = `Perímetro del rectángulo: ${perimeter.toFixed(2)}`;
+            diagonalShow.innerHTML = `Diagonal del rectángulo: ${diagonal.toFixed(2)}`;
+        };
     });
 });
